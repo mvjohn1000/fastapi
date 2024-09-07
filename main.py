229dd3c8989fcd2f4e8929b38711fbdf75fcb50f
M V John <mvjohn100@gmail.com>
2024-09-07 13:46:51 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException, Response
+
+app = FastAPI()
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found", headers={"X-Error": "There goes my error"})
+    return {"item": items[item_id]}
+
+
+@app.get("/hello")
+def read_root():
+    response = Response(content="Hello, world!", media_type="text/plain")
+    response.headers["X-Custom-Header"] = "My custom header"
+    return response
+    # return {"name": "john"}
